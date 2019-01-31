bc35747508b2352195b9ec808af813b0d8bdb86c
Saitarun Irigala <saitarun@mail.usf.edu>
2019-01-31 17:00:39 -0500

    v1.0

--- SIrigala_Array_Deliverable/Program.cs
+++ SIrigala_Array_Deliverable/Program.cs
@@ -4,9 +4,19 @@ namespace SIrigala_Array_Deliverable
 {
     class Program
     {
+        static int[] array;
+
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello World!");
-        }
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                                20, 21, 22, 23, 24, 25 }; //define array
+            foreach (int i in numbers)
+            {
+                Console.WriteLine("Element Value: " + i + ".");
+            }//foreach complete
+            Console.ReadKey(true);
+        } 
+        
     }
 }
